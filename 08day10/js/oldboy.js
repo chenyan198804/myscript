4207ee4b548f3b2c4d49c9870810134cebe427f0
Candy <chenyanxx@126.com>
2017-02-13 15:01:38 +0800

    update 08day10 content

---
+++ 08day10/js/oldboy.js
@@ -0,0 +1,89 @@
+//alert('理解');
+/*
+多行注释
+
+
+name='123' //全局变量
+//var name='alex' 局部变量
+
+Foo()
+Bar()
+
+function Foo(){
+    //var name = 'alex'
+    name='alex';var age = 19;
+    //var arg2 = arguments[1];
+    console.log(name);
+    //console.log(arg2);
+
+    return xxx;
+}
+
+function Bar(){
+    console.log(name);
+}
+
+
+//匿名函数，写的不多
+var temp = function(){
+
+}
+
+//自执行函数
+(function(name){ //形式参数
+    console.log(name);
+})('Hi,Candy') //实参
+
+//声明
+var array = [1,2,3,4];
+//var array = Array(1,2,3,4,5);
+
+//添加
+array.push('alex'); //追加
+console.log(array);
+
+array.unshift('old');//最前插入
+console.log(array);
+
+array.splice(1,0,'boy');//obj.splice(index,0,'content')
+console.log(array);
+
+array.splice(100,0,'ok');
+console.log(array);
+//数组可以使不连续的
+array[100]='nice'
+console.log(array[99]);
+console.log(array[100]);
+
+//删除
+array.pop('nice');
+console.log(array);
+array.shift('boy');
+console.log(array);
+array.splice(1,2);//obj.splice(index,count)
+console.log(array);
+
+//截取
+name = [1,2,3,4,5,6,7]
+
+*/
+
+/*
+var array = [11, 22, 33, 44, 55];
+var dict = {'name':'alex','age':18}
+
+for(var item in array){
+    console.log(item);
+
+}
+for(var item in dict){
+    console.log(item);
+    console.log(dict[item]);
+}
+
+
+for(var i=0;i<10;i++){
+    console.log(array[i]);
+}
+
+*/
